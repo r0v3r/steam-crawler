43392cf626c6bd2701a7c7f3777726152c189f3f
rover <874413503@qq.com>
2017-11-20 22:18:42 +0800

    add simple crawler

---
+++ steam_crawler.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['http://store.steampowered.com/search/?filter=topsellers&page=1']
+
+    def parse(self, response):
+        for item in response.css('a.search_result_row'):
+            # yield {'name': item.css('span.title ::text').extract_first(), 'price': item.css('div.search_price ::text')}
+            yield {'game': item.css('span.title ::text').extract_first(), 'price': item.css('div.search_price ::text').extract_first()}
+            # print {'title': title.css('a ::text').extract_first()}
+
+        for next_page in response.css('a.pagebtn'):
+            yield response.follow(next_page, self.parse)
