d7643ee2e56f738865f4064a85608562f0dde305
rover <874413503@qq.com>
2017-11-21 09:21:23 +0800

    update crawler: crawl search page, almost 10000 games

--- steam_crawler.py
+++ steam_crawler.py
@@ -2,13 +2,14 @@ import scrapy
 
 class SteamCrawler(scrapy.Spider):
     name = 'steamcrawler'
-    start_urls = ['http://store.steampowered.com/search/?filter=topsellers&page=1']
+    start_urls = ['http://store.steampowered.com/search/?page=1']
 
     def parse(self, response):
         for item in response.css('a.search_result_row'):
-            # yield {'name': item.css('span.title ::text').extract_first(), 'price': item.css('div.search_price ::text')}
-            yield {'game': item.css('span.title ::text').extract_first(), 'price': item.css('div.search_price ::text').extract_first()}
-            # print {'title': title.css('a ::text').extract_first()}
-
+            yield {'game': item.css('span.title::text').extract_first(),\
+            'img': item.css('div.search_capsule img::attr(src)').extract_first(),\
+            'release': item.css('div.search_released::text').extract_first(),\
+            'price': item.css('div.search_price').extract_first()}
+       
         for next_page in response.css('a.pagebtn'):
             yield response.follow(next_page, self.parse)
